c61650a762439c28b5e5c0beedf015dbc1dc0b34
Pedro Ovidio <pedrovidio@gmail.com>
2020-11-06 15:51:24 -0300

    seguranca

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://gol.sphinxweb.com.br/auditoria';
+$config['base_url'] = 'https://gol.sphinxweb.com.br/auditoria/';
 
 /*
 |--------------------------------------------------------------------------
